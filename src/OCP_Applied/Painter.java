91db10dec7b906a92136a73cee6130b180b99e59
ibwoy <ibraimsap@gmail.com>
2016-02-08 15:05:56 +0200

    implemented classes

--- src/OCP_Applied/Painter.java
+++ src/OCP_Applied/Painter.java
@@ -1,7 +1,26 @@
 package OCP_Applied;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 /**
  * Created by io on 2/7/16.
  */
-public class Painter {
+public class Painter extends JPanel {
+    private ArrayList shapes;
+
+    public Painter(ArrayList shapesToDraw) {
+        shapes = shapesToDraw;
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        for(int i=0; i<shapes.size(); i++)
+        {
+            Shape shape = (Shape)shapes.get(i);
+            shape.draw(g);
+        }
+    }
 }
--- src/OCP_Applied/ShapeApplication.java
+++ src/OCP_Applied/ShapeApplication.java
@@ -1,7 +1,35 @@
 package OCP_Applied;
 
+import javax.swing.*;
+import java.util.ArrayList;
+
 /**
  * Created by io on 2/7/16.
  */
-public class ShapeApplication {
+public class ShapeApplication extends JFrame{
+
+    private Painter canvas;
+
+    public ShapeApplication() {
+        Square S1 = new Square(50, 50, 50);
+        Circle C1 = new Circle(100, 100, 40);
+        Circle C2 = new Circle(200, 100, 20);
+        ArrayList shapes = new ArrayList();
+        shapes.add(S1);
+        shapes.add(C1);
+        shapes.add(C2);
+
+        canvas = new Painter(shapes);
+
+        setContentPane(canvas);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        ShapeApplication app1 = new ShapeApplication();
+    }
+
 }
